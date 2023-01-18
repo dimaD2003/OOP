fb9e62c17783669237ec3d810be1c9282af9ce0a
dimaD2003 <dimamoiseev20031707@yandex.ru>
2023-01-18 22:22:23 +1000

    tests

--- task_3/tests.cpp
+++ task_3/tests.cpp
@@ -25,3 +25,19 @@ TEST(Translate, CodeChar)
 
         EXPECT_EQ(FullandString(111,8), "00000111");
 }
+TEST(Encode2, EncodeStr2)
+{
+        std::string s ="MAN";
+         std::ostringstream out;
+        encode(s,out);
+
+        EXPECT_EQ(out.str(), "TUFO");
+}
+TEST(DEcode2, DEcodeStr2)
+{
+        std::string s ="TkFOTw==";
+         std::ostringstream out;
+        base64_decode(s,out);
+
+        EXPECT_EQ(out.str(), "NANO");
+}
