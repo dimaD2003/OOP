b53b20fd3b81509bf91411ad0709fb7b7dc771db
dimaD2003 <113402321+dimaD2003@users.noreply.github.com>
2023-03-27 19:26:19 +1000

    Delete task_2 directory

--- task_2/build/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/OOP/task_2/googletest/googletest
-# Build directory: /root/OOP/task_2/build/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- task_2/src/Functions/funct.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <string>
-std::string input(std::string s);
-std::string DeleteSpace(std::string &s);
-void output(std::string s,std::ostream &os);
-std::string hashtagMaker(std::string s);
